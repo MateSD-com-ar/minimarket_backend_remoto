18f1d356075840cc8d27afa867c8f267f31e1623
HansUrpay <hansurpayh@gmail.com>
2024-07-20 13:39:23 -0500

    feat: configuration finished

--- src/main/java/com/virgen_lourdes/minimarket/auth/RegisterRequest.java
+++ src/main/java/com/virgen_lourdes/minimarket/auth/RegisterRequest.java
@@ -1,6 +1,8 @@
 package com.virgen_lourdes.minimarket.auth;
 
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 /**
@@ -14,8 +16,10 @@ import lombok.*;
 public class RegisterRequest {
 
     @NotBlank(message = "El nombre de usuario es requerido")
+    @Size(min = 3, message = "El nombre de usuario debe tener al menos 3 caracteres")
     private String username;
 
     @NotBlank(message = "La contraseña es requerida")
+    @Size(min = 4, message = "La contraseña debe tener al menos 4 caracteres")
     private String password;
 }
