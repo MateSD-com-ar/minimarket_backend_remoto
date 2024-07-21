2856a413c04e99a476e430f4aa5b44342543f80a
rubenledesma10 <rubenledesma89@gmail.com>
2024-07-21 17:08:11 -0300

    saledetailsproduct entity and repository

---
+++ src/main/java/com/virgen_lourdes/minimarket/entity/SaleDetailsProduct.java
@@ -0,0 +1,31 @@
+package com.virgen_lourdes.minimarket.entity;
+
+import com.virgen_lourdes.minimarket.validation.ValidProduct;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "saledetailsproduct")
+public class SaleDetailsProduct {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idDetails;
+    private Double amount;
+    private Double unitPrice;
+    private Double totalPriceDetail;
+    @ManyToOne
+    @JoinColumn(name = "sale_id", nullable = false)
+    private List<Sale> sale;
+    @ManyToOne
+    @JoinColumn(name = "id_product", nullable = false)
+    private List<Product> product;
+
+}
