0b53750b17f6bc6cafcd01a18e50d3ada33f0a1a
rubenledesma10 <rubenledesma89@gmail.com>
2024-09-19 14:04:12 -0300

    commit

--- minimarket_virgen_lourdes_backend/src/main/java/com/virgen_lourdes/minimarket/controller/HealthCheckController.java
+++ minimarket_virgen_lourdes_backend/src/main/java/com/virgen_lourdes/minimarket/controller/HealthCheckController.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+/*
 @RestController
 public class HealthCheckController {
 
@@ -21,3 +21,4 @@ public class HealthCheckController {
         }
     }
 }
+*/
--- minimarket_virgen_lourdes_backend/src/main/java/com/virgen_lourdes/minimarket/utils/ServerWakeUpScheduler.java
+++ minimarket_virgen_lourdes_backend/src/main/java/com/virgen_lourdes/minimarket/utils/ServerWakeUpScheduler.java
@@ -5,10 +5,10 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
+/*
 
-/**
  * This class is responsible for scheduling tasks to wake up the server at specific times.
- */
+
 @Component
 @EnableScheduling
 public class ServerWakeUpScheduler {
@@ -18,12 +18,12 @@ public class ServerWakeUpScheduler {
 
     private final RestTemplate restTemplate = new RestTemplate();
 
-    /**
+
      * This method is scheduled to run every 13 minutes (780,000 milliseconds).
      * It wakes up the server by sending a GET request to the URL configured in the env.properties file.
      * If the request is successful, it prints a message indicating that the server has woken up.
      * If an exception occurs, it prints an error message with the exception details.
-     */
+
     @Scheduled(fixedRate = 780000, initialDelay = 780000)
     public void wakeUpServer() {
         String url = serverUrl + "/health";
@@ -35,3 +35,4 @@ public class ServerWakeUpScheduler {
         }
     }
 }
+*/
