893686dd6236c8656e7f6a05554f2b014fb88d33
HansUrpay <hansurpayh@gmail.com>
2024-09-25 22:23:54 -0500

    refactor(Cors): add environment variable of FRONTEND_URL for CORS

--- src/main/java/com/virgen_lourdes/minimarket/config/CorsConfig.java
+++ src/main/java/com/virgen_lourdes/minimarket/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.virgen_lourdes.minimarket.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -9,12 +10,15 @@ import org.springframework.web.filter.CorsFilter;
 @Configuration
 public class CorsConfig {
 
+    @Value("${FRONTEND_URL}")
+    private String frontendUrl;
+
     @Bean
     public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("http://localhost:3000");
+        config.addAllowedOrigin(frontendUrl);
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
         source.registerCorsConfiguration("/**", config);
